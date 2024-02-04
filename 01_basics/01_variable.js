eeec359d375f16391400db3473aec5839f3368f6
Aritra Sarkar <aritra.aiub@gmail.com>
2024-02-04 15:27:42 +0600

    first commit

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,7 @@
+const accountId = 100100;
+let accountEmail = "saritra33@gmail.com";
+var accountPassword = "12345";
+accountCity = "Dhaka";
+let accountState;
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict";
+
+let name = "Aritra";
+let age = 26;
+let isLoggedIn = false;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof undefined);
